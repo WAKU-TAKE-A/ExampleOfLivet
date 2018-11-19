0c7b3d32f282630b6863f32c3e76ba047731cedc
TakehitoNishida <takehito.nishida@gmail.com>
2018-11-19 21:54:34 +0900

    update

--- ExampleOfLivet/__init__.py
+++ ExampleOfLivet/__init__.py
@@ -4,12 +4,12 @@
 Livet for IronPython.
 
 * Environment variable 'IRONPYTHON_HOME' is required. It is the installation location of IronPython.
-* "Livet.dll" is required.
+* "Livet.dll" is required. The version is 1.3.
 """
 
 __author__  = "Nishida Takehito <takehito.nishida@gmail.com>"
-__version__ = "0.9.0.0"
-__date__    = "2018/10/10"
+__version__ = "0.9.1.0"
+__date__    = "2018/10/17"
 
 #
 # append path.
@@ -23,26 +23,20 @@ IRONPYTHON_HOME = env.GetEnvironmentVariable("IRONPYTHON_HOME")
 if IRONPYTHON_HOME is None:
     raise Exception("Error : Set path of IRONPYTHON_HOME.")
 
-IRONPYTHON_LIB = path.join(IRONPYTHON_HOME, "Lib")
-IRONPYTHON_DLLS = path.join(IRONPYTHON_HOME, "DLLs")
-IRONPYTHON_LIVET = path.join(IRONPYTHON_HOME, "Lib\\livet")
+IPY_LIB = path.join(IRONPYTHON_HOME, "Lib")
+IPY_DLLS = path.join(IRONPYTHON_HOME, "DLLs")
+IPY_LIVET = path.join(IRONPYTHON_HOME, "Lib\\livet")
 
 _lstPath = []
-_lstPath.append(IRONPYTHON_LIB)
-_lstPath.append(IRONPYTHON_DLLS)
-_lstPath.append(IRONPYTHON_LIVET)
-
-_checkPath = True
+_lstPath.append(IPY_LIB)
+_lstPath.append(IPY_DLLS)
+_lstPath.append(IPY_LIVET)
 
 for i in _lstPath:
     if os.path.exists(i):
         systemPath.append(i)
     else:
-        _checkPath = False
-        print("There is no '" + i + "'.")
-
-if _checkPath == False:
-	raise Exception("Error occured.")
+        raise Exception("There is no '" + i + "'.")
 
 #
 # Import modules.
